f65488539aa5620722890ec3d9c30803f15e5504
Ardhanari <genevril@gmail.com>
2019-11-20 17:39:23 +0000

    product model added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=254, default='')
+    image = models.ImageField(upload_to='images')
+    description = models.TextField()
+    category = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    discount = models.DecimalField(max_digits=2, decimal_places=0)
+    # date_added = 
+
+    def __str__(self):
+        return self.name
