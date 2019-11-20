45e482114502b41287dee3bb964d088efd375814
Ardhanari <genevril@gmail.com>
2019-11-20 14:45:29 +0000

    signup form validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,14 +9,30 @@ class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-class SignUpNewUser(forms.Form):
+class SignUpNewUserForm(UserCreationForm):
     """Form used to create new instance of user """
 
-    # username = forms.CharField()
-    # email = 
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm your password")
-
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm your password", widget=forms.PasswordInput)
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError("Email address must be unique")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError("You must fill both fields")
+        
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, SignUpNewUserForm
 
 def index(request):
     """Return index.html file"""
@@ -43,4 +43,6 @@ def login(request):
 def signup(request):
     """Returns sign up form"""
 
-    return render(request, 'signup.html')
+    signup_form = SignUpNewUserForm()
+
+    return render(request, 'signup.html', {'signup_form': signup_form})
