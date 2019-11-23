8daa04dc8c88004e92842d07085b9a556da020b8
Ardhanari <genevril@gmail.com>
2019-11-23 16:05:46 +0000

    small improvement to Product model

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 class ProductCategory(models.Model):
     """Category of the product"""
@@ -16,20 +17,19 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     """Single product"""
+
     name = models.CharField(max_length=254, default='', blank=False)
     image = models.ImageField(upload_to='images')
     description = models.TextField()
-    variant = models.CharField(max_length=20, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField()
-    # status
-    # created_at = models.DateField()
+    created_at = models.DateTimeField(default=timezone.now)
     discount = models.DecimalField(max_digits=2, decimal_places=1, default=0)
     category = models.ForeignKey(ProductCategory)
     is_digital = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name    
+        return self.name
 
 class ProductReview(models.Model):
     """Defines reviews given by users to products they purchased"""
