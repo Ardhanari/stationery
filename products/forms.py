6b0f6b3c9c7df8f7d782b1541a1b1b0ff1c640e4
Ardhanari <genevril@gmail.com>
2019-11-23 18:55:49 +0000

    products review form added

---
+++ products/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import ProductReview
+
+class ProductReviewForm(forms.ModelForm):
+
+    author = User.username
+
+    class Meta:
+        model = ProductReview
+        fields = ('title', 'review', 'rating')
