6a934543b42daafcae34227bb4c53d1a05f02a50
Ardhanari <genevril@gmail.com>
2019-11-23 15:45:22 +0000

    basic model testing for Orders

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.utils import timezone
 # import custom user model too, when it's ready
 
 class Order(models.Model):
@@ -12,10 +13,10 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=True)
-    # date = models.DateField()
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return "({0}) {1} - {2}".format(self.id, self.full_name, self.country)
+        return "({0}) {1} - {2}, {3}".format(self.id, self.date, self.full_name, self.country)
 
 
 class OrderLineItem(models.Model):
