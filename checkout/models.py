0e5a523d6150e87bea71b71aa6867dacf41fdbdb
Ardhanari <genevril@gmail.com>
2019-11-23 16:16:37 +0000

    django_countries installed and replacing old country field

--- checkout/models.py
+++ checkout/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from products.models import Product
 from django.utils import timezone
+from django_countries.fields import CountryField
+
 # import custom user model too, when it's ready
 
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     company = models.CharField(max_length=50, blank=True)
     phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False) # to be replaced by country values
+    country = CountryField()
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
-    county = models.CharField(max_length=40, blank=True)
+    county = models.CharField(max_length=40, blank=False)
     date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return "({0}) {1} - {2}, {3}".format(self.id, self.date, self.full_name, self.country)
+        return "({0}) {1} - {2}, {3}".format(self.id, self.date, self.full_name, self.country.name)
 
 
 class OrderLineItem(models.Model):
