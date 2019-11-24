bedf2b3ed1c9b569417c0d690514c0e98c837daf
Ardhanari <genevril@gmail.com>
2019-11-24 14:38:02 +0000

    single product page added

--- products/views.py
+++ products/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render, redirect, reverse
 from .models import Product
 
 def all_products(request):
-    products = Product.objects.all()
+    """renders page with all available products, excluding these not available for buying"""
+    products = Product.objects.all().exclude(quantity=0)
     return render(request, 'allproducts.html', {'products': products})
+
+def single_product(request, id):
+    """renders single product page with detailed information"""
+    chosen_product = Product.objects.get(id=id)
+    return render(request, 'singleproduct.html', {'chosen_product': chosen_product})
