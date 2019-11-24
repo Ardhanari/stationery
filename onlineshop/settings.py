235988744e7e7b6937d3ffbbec92308465221cc1
Ardhanari <genevril@gmail.com>
2019-11-24 01:38:45 +0000

    no env import

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env # commented out for heroku
+#import env # commented out for heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
