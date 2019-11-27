637d4c4b0279330275a5d56d055b59ade2cebce4
Ardhanari <genevril@gmail.com>
2019-11-27 23:36:14 +0000

    changed relation between ShippingAddress and User

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
 class ShippingAddress(models.Model):
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User, primary_key=True)
     full_name = models.CharField(max_length=50, blank=False)
     company = models.CharField(max_length=50, blank=True)
     street_address1 = models.CharField(max_length=40, blank=False)
@@ -13,3 +13,6 @@ class ShippingAddress(models.Model):
     county = models.CharField(max_length=40, blank=True)
     country = CountryField()
     phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return "{0}\n{1}\n{2}\n{3}\n{4}".format(self.full_name, self.street_address1, self.street_address2, self.town_or_city, self.country.name)
