f50b03724765a725c76c722c2e01634f94d4af47
Ardhanari <genevril@gmail.com>
2019-11-30 13:49:46 +0000

    heroku 500 error fix

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEVELOPMENT = os.getenv('DEBUG')
+DEVELOPMENT = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = [os.environ.get('HOST'),
                  'https://stationery-shop-1st-depl.herokuapp.com/']
